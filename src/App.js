dacbd193ee89100913218619ce0677725d1051fe
joaco1975 <ingjjvbravo1975@gmail.com>
2022-07-13 12:55:19 -0500

    first commit prueba joaco

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Pizzas } from './componets/Pizzas';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>menu de pizzas</h1>
+      <Pizzas></Pizzas>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componets/Pizzas.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Pizzas = () => {
+  return (
+    <> <h1>pedir pizzas</h1>
+    <button> pedir pizzza ahora</button>
+    </>
+
+  )
+}
